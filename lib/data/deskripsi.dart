228e13a341ce6f221b39a0670e1d972dd7df8f1b
adeliaokt <adeliaaa.ok@gmail.com>
2022-08-09 21:13:13 +0700

    first commit

---
+++ lib/data/deskripsi.dart
@@ -0,0 +1,72 @@
+class resep {
+  String name, harga, detail, image;
+
+  resep(
+      {required this.name,
+      required this.harga,
+      required this.detail,
+      required this.image});
+}
+
+List<resep> dataResep = [
+  resep(
+      name: 'HMNS Perfume ORGASM',
+      harga: 'Rp.312.000-351.000',
+      detail:
+          'Aroma parfum ini didominasi floral notes yang memberi kesan rich, luxury, dan romantic. Soal ketahanan, parfum lokal ini memiliki ketahanan hingga 6 jam. Aroma wanginya bahkan dapat tercium dari jarak 2 meter.',
+      image: 'assets/1.jpg'),
+  resep(
+      name: 'Oullu Zephyr',
+      harga: 'Rp. 189.000-Rp. 389.000',
+      detail:
+          'Oullu varian parfum Zephyr memiliki wangi yang floral, manis, dan segar cocok untuk disemprot di pagi hari sebelum beraktivitas. Parfum ini juga pas digunakan saat nge-date untuk memberikan impresi yang baik pada lawan kencanmu. Dikemas dalam botol yang mudah untuk dibawa kemana-mana, ada pilihan ukuran 15ml yang bisa kamu coba terlebih dahulu sebelum membeli ukuran 50ml.',
+      image: 'assets/2.jpg'),
+  resep(
+      name: 'LAUX Parfume\nCookies & Cream',
+      harga: 'Rp.59.000-Rp.87.000',
+      detail:
+          'Laux Perfume Cookies and Cream cocok untuk pria maupun wanita sebagai parfum sehari-hari yang akan memberikan keharuman di setiap kegiatan harimu. Dengan aroma vanilla cake, calm, powdery, balsamic, dan creamy secara bersamaan, parfum yang satu ini akan membuat kamu langsung jatuh cinta dengan aromanya.',
+      image: 'assets/3.jpg'),
+  resep(
+      name: 'Carl & Claire Gorgeous\nEau De Parfum',
+      harga: 'Rp.119.000-Rp.359.000',
+      detail:
+          'Carl & Claire Gorgeous Eau De Parfum memiliki sensasi seperti segar setelah mandi di siang bolong. Parfum wanita ini memiliki aroma fruity dan citrus yang khas, Toppers. Tentunya, parfum ini cocok buat kamu yang memiliki ciri khas ceria sekaligus elegan. Parfum varian ini memiliki Base notes Moss, Musky, dan Oriental yang tahan hingga 8–10 jam.',
+      image: 'assets/4.png'),
+  resep(
+      name: 'Saff & Co varian Omnia',
+      harga: 'Rp.176.600-Rp.179.900',
+      detail:
+          'Manjakan diri dengan keharuman bunga muda OMNIA yang akan membuat kamu tetap energik dan juga wangi sepanjang hari. Didesain untuk memikat banyak orang, SAFF & Co. OMNIA merupakan parfum yang ideal dengan karakter powdery, sweet, dan musk, untuk meninggalkan kesan lembut namun berbunga-bunga setiap harinya. Parfum ini juga dapat bertahan hingga enam jam.',
+      image: 'assets/5.jpg'),
+  resep(
+      name: 'Evenium Vanilla Rose',
+      harga: 'Rp.28.500-Rp.152.150',
+      detail:
+          'Evenium Vanilla Rose cocok buat kaum Hawa yang menyukai wangi yang lembut dan manis dari paduan aroma rose dengan vanilla oriental. Wanginya yang tahan lama mampu menunjang aktivitas kamu seharian. Evenium Vanilla Rose adalah salah satu parfum yang memiliki genre Sweet Romantic Gourmand sehingga cocok digunakan untuk jalan-jalan bersama orang tersayang. Parfum ini hadir dalam travel size dan full size sebesar 50 ml.',
+      image: 'assets/6.jpg'),
+  resep(
+      name: 'Kahf Invigorating Waterfall\nEau de Toilette',
+      harga: 'Rp. 70.000',
+      detail:
+          'Parfum kerap dikemas dalam botol yang cukup bulky. Namun, kemasan Kahf Invigorating Waterfall EDT yang satu ini sangat travel-friendly. Ukuran parfum ini praktis masuk dalam kantung Anda dan mudah dibawa ke mana saja. Parfum pria ini menawarkan aroma bernuansa aquatic yang dipadukan dengan citrus dan musk.',
+      image: 'assets/7.jpg'),
+  resep(
+      name: 'Onix Scorpio',
+      harga: 'Rp. 180.000',
+      detail:
+          'Parfum pria berikut ini terdiri dari wangi citrus dan rempah yang disusul dengan wangi floral dan kopi. Tak hanya itu, parfum pria ini juga menambahkan aroma bernuansa woody yang hangat.  Wangi parfum pria Onix Scorpio ini juga manis dan elegan tidak terlalu strong sehingga cocok digunakan sehari-hari.',
+      image: 'assets/8.jpg'),
+  resep(
+      name: 'OUDS Parfume NUSA',
+      harga: 'Rp. 150.000',
+      detail:
+          'Rempah Indonesia memiliki aroma yang eksotis. Hal itu, tertuang dalam OUDS varian NUSA. Parfum pria ini menghadirkan perpaduan aroma gaharu dan beberapa wangi floral, seperti chamomile dan Bulgarian rose. Aroma yang unik ini dikemas dalam konsentrasi parfum yang kuat sehingga memberikan ketahanan yang lama. Anda bisa menikmati wangi parfum pria ini hingga 6 jam.',
+      image: 'assets/9.jpg'),
+  resep(
+      name: 'Labcitane Coffee Mood',
+      harga: 'Rp. 185.000',
+      detail:
+          'Jangan tertipu dengan judul coffee parfum yang satu ini. Meski menuliskan kata coffee, parfum ini tidak memberikan aroma kopi yang kuat seperti parfum ruangan. Parfum unisex ini memadukan kopi dan vanila yang menghasilkan nuansa aroma yang manis. Aroma ini menciptakan kesan misterius dan klasik bagi pemakainya.',
+      image: 'assets/10.jpg'),
+];
